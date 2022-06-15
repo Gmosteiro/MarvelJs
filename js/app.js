97fc54f2a3bc03a9dcb8478fc1d345aacfd7f759
Gmosteiro <gastonmosteiro@hotmail.com>
2022-06-14 23:45:12 -0300

    Trae personajes

--- js/app.js
+++ js/app.js
@@ -2,14 +2,26 @@
 
 const marvel = {
     render: ()=> {
-        const URLAPI = "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=af09c13931ea272816d746655e13c3fb&hash=B4DDC8E1418CB203E8878A50FB2670D4";
+        const URLAPI = 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=af09c13931ea272816d746655e13c3fb&hash=c55576355380d9ba4039ebaf3b031bed';
         const container = document.querySelector('#marvel-row');
         let contentHTML = '';
 
         fetch(URLAPI)
         .then (res => res.json())
         .then((json) => {
-            console.log(json, 'RES.JSON')
+            for(const pj of json.data.results){
+                //console.log(json, 'RES.JSON')
+                let urlpj = pj.urls[0].url;
+                contentHTML += `
+                    <div class="col-md-4">
+                        <a href="${urlpj}" target="_blank">
+                            <img src="${pj.thumbnail.path}.${pj.thumbnail.extension}" alt="${pj.name}" class="img-thumbnail">
+                        </a>
+                        <h3 class="title">${pj.name}</h3>
+                    </div>`
+            }
+            container.innerHTML=contentHTML;
+            
         })  
     }
 };
